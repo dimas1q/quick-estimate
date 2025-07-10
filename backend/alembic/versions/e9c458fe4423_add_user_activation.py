9f5de0bee977d4e2f860c4d96da6c195671fd085
Dmitriy Shitov <shitov.d@gitflic-team.ru>
2025-07-10 18:14:11 +0300

    feat: add user activation fields and configure Mailhog for SMTP

--- backend/alembic/versions/e9c458fe4423_add_user_activation.py
+++ backend/alembic/versions/e9c458fe4423_add_user_activation.py
@@ -1,16 +1,17 @@
 """add user activation fields
 
 Revision ID: e9c458fe4423
-Revises: fb212cb36ab6
+Revises: 0123456789ab
 Create Date: 2025-07-01 12:00:00
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
 revision: str = "e9c458fe4423"
-down_revision: Union[str, None] = "fb212cb36ab6"
+down_revision: Union[str, None] = "0123456789ab"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
